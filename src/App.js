220e2b44572ed885243450d32f06975388d4800b
vikash <vikash.tomar.india@gmail.com>
2023-08-16 17:20:25 +0530

    first added counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+
+import React, { useState } from "react";
+import "./style.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [quote,setQuote] = useState([])
+
+  function inc(e) { 
+    console.log(count)
+    e.preventDefault();    
+    setCount(count + 1);
+    if(count==4)
+    {
+       let quote = myapi()
+      
+    }
+  }
+
+  function dec() {
+     
+     setCount(count > 0 ? count - 1 : 0)
+  }
+
+  let api =  "https://api.quotable.io/quotes/random"
+
+
+  function myapi(){
+    axios.get(api)
+    .then((res)=>{
+      console.log(res)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="counter-widget">
+      <div className="count">{count}</div>
+      <button className="increment" onClick={inc}>
+        Increment
+      </button>
+      { (count==0)?
+      <button className="decrement" disabled  onClick={dec}>
+
+        
+        Decrement
+      </button>:<button className="decrement"  onClick={dec}>
+
+        
+Decrement
+</button>}
+
+          <div>
+
+               
+
+             {/* <div className="quote">Random Quote: {quote}</div> */}
+          </div>
+
+  </div>
+
+        
+
   );
 }
 
